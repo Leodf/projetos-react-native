821f0ead37a1b1692d48b7bb57e1df658b6293a3
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-06-27 17:53:16 -0300

    criando o primeiro componente

---
+++ exercicios/src/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Text } from 'react-native'
+
+function App() {
+    const jsx = <Text>Olá mundo!! meu primeiro componente</Text>
+
+    return jsx
+}
+
+export default App
