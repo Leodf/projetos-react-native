5d9afa136417de534f3ef23c1d741d2e7c710c54
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-07-07 16:35:59 -0300

    terminando o projeto numero aleatorios mega sena

--- exercicios/src/components/mega/Mega.js
+++ exercicios/src/components/mega/Mega.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Button, Text, TextInput } from 'react-native'
+import { Button, Text, TextInput, View } from 'react-native'
 import Estilo from '../estilos'
+import MegaNumero from './MegaNumero'
 
 export default class Mega extends Component {
 
@@ -26,6 +27,13 @@ export default class Mega extends Component {
         this.setState({ numeros })
     }
 
+    exibirNumeros = () => {
+        const nums = this.state.numeros
+        return nums.map(num => {
+            return <MegaNumero key={num} num={num} />
+        })
+    }
+
     render() {
         return (
             <>
@@ -43,9 +51,15 @@ export default class Mega extends Component {
                 title='Gerar'
                 onPress={this.gerarNumeros}
             />
-            <Text style={Estilo.fontG}>
-                {this.state.numeros.join(',')}
-            </Text>
+            <View style={{
+                marginTop: 20,
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+                justifyContent: 'center',
+            }}>
+                {this.exibirNumeros()}
+            </View>
+            
             </>
         )
     }
