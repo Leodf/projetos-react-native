0d77cc1993da7941874c513545f2877efd349ef6
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-07-07 15:54:54 -0300

    criando o setState

--- exercicios/src/components/mega/Mega.js
+++ exercicios/src/components/mega/Mega.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native'
+import { Text, TextInput } from 'react-native'
 import Estilo from '../estilos'
 
 export default class Mega extends Component {
 
+    state = {
+        qtdeNumeros: this.props.qtdeNumeros
+    }
+
+    alterarQtdeNumero(qtde) {
+        this.setState({ qtdeNumeros: qtde })
+    }
+
     render() {
         return (
+            <>
             <Text style={Estilo.fontG}>
                 Gerador de Mega-Sena 
-                {this.props.qtdeNumeros}
+                {this.state.qtdeNumeros}
             </Text>
+            <TextInput
+                placeholder='Qtde de Números'
+                value={this.state.qtdeNumeros}
+                onChangeText={this.alterarQtdeNumero}
+            />
+            </>
         )
     }
 }
